33940cd6aa23bd1abe9198eb4f26378b86d38609
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-12-02 20:26:13 +0100

    New Password Entity and Form implementations

--- src/main/java/pl/konradboniecki/Budget/models/newPassword/NewPassword.java
+++ src/main/java/pl/konradboniecki/Budget/models/newPassword/NewPassword.java
@@ -1,6 +1,28 @@
 package pl.konradboniecki.Budget.models.newPassword;
 
-public class newPassword {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table
+@Data
+public class NewPassword {
+    @Id
+    @Column(name="account_id")
+    private Long id;
     
+    @Column(name="new_password")
+    private String nextPassword;
+    @Column(name="apply_time")
+    private ZonedDateTime ApplyTime;
     
+    public NewPassword(){;}
+    
+    public NewPassword(NewPasswordForm newPasswordForm){
+        setId(newPasswordForm.getAccount_id());
+        setApplyTime(ZonedDateTime.now());
+        setNextPassword(newPasswordForm.getPassword());
+    }
 }
