83648e6dba0559b8acf00832276ac4d70fd1d895
Konrad Boniecki <konrad_boniecki@hotmail.com>
2018-07-14 16:24:34 +0200

    Added FamilyTest Class

---
+++ models/src/test/java/pl/konradboniecki/models/account/FamilyTest.java
@@ -0,0 +1,18 @@
+package pl.konradboniecki.models.account;
+
+public class FamilyTest {
+
+
+    public void testEmptyConstructor(){
+
+    }
+    public void test2(){
+
+    }
+    public void test3(){
+
+    }
+    public void test4(){
+
+    }
+}
