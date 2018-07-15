c9f22f88cf87ce53ac0739f292ba30c7d2dd119f
Konrad Boniecki <konrad_boniecki@hotmail.com>
2018-07-15 13:25:56 +0200

    added frontendforms test scratches.

--- models/src/test/java/pl/konradboniecki/models/frontendforms/FamilyCreationFormTest.java
+++ models/src/test/java/pl/konradboniecki/models/frontendforms/FamilyCreationFormTest.java
@@ -1,5 +1,6 @@
 package pl.konradboniecki.models.frontendforms;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -10,11 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @TestInstance(Lifecycle.PER_CLASS)
 public class FamilyCreationFormTest {
 
+    @Disabled
     @Test
     void initTest(){
         FamilyCreationForm familyCreationForm = new FamilyCreationForm();
         assertNull(familyCreationForm.getTitle());
 
+        //TODO: validate input against regex
         familyCreationForm.setTitle("test_title");
         assertEquals("test_title", familyCreationForm.getTitle());
     }
