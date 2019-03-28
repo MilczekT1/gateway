0982a00082ed9fb07fe66d3b5795be46d6eef3b1
Konrad.Boniecki <konrad.boniecki@finastra.com>
2019-03-28 13:00:17 +0100

    removed activation code with repository and replaced with untested http request

--- webservice-mvc/src/main/java/pl/konradboniecki/BudgetApp.java
+++ webservice-mvc/src/main/java/pl/konradboniecki/BudgetApp.java
@@ -2,6 +2,8 @@ package pl.konradboniecki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BudgetApp {
@@ -9,4 +11,9 @@ public class BudgetApp {
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetApp.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+	    return new RestTemplate();
+    }
 }
--- webservice-mvc/src/main/java/pl/konradboniecki/webservices/mvc/auth/RegisterController.java
+++ webservice-mvc/src/main/java/pl/konradboniecki/webservices/mvc/auth/RegisterController.java
@@ -5,18 +5,21 @@ import com.google.common.base.Throwables;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import pl.konradboniecki.models.account.Account;
 import pl.konradboniecki.models.account.AccountRepository;
 import pl.konradboniecki.models.frontendforms.AccountForm;
-import pl.konradboniecki.models.useractivationcode.UserActivationCode;
-import pl.konradboniecki.models.useractivationcode.UserActivationCodeRepository;
 import pl.konradboniecki.utils.BudgetAdress;
 import pl.konradboniecki.utils.RestCall;
 import pl.konradboniecki.utils.TokenGenerator;
@@ -25,6 +28,7 @@ import javax.validation.Valid;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static java.util.Collections.singletonList;
 import static pl.konradboniecki.templates.ViewTemplate.*;
 import static pl.konradboniecki.utils.enums.ErrorType.PROCESSING_EXCEPTION;
 
@@ -35,9 +39,9 @@ public class RegisterController {
     @Autowired
     private AccountRepository accountRepository;
     @Autowired
-    private UserActivationCodeRepository userActivationCodeRepository;
-    @Autowired
     private RestCall restCall;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @PostMapping("/register")
     public ModelAndView register(@ModelAttribute("accountFormObject") @Valid AccountForm newAccountForm,
@@ -55,7 +59,21 @@ public class RegisterController {
             acc = accountRepository.findByEmail(acc.getEmail()).get();
             String token = new TokenGenerator().createUUIDToken();
             //TODO: rewrite below line this to use service
-            userActivationCodeRepository.save(new UserActivationCode(acc.getId(), token));
+            //===================================================
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(singletonList(MediaType.APPLICATION_JSON_UTF8));
+            headers.set("id", acc.getId().toString());
+
+            HttpEntity httpEntity = new HttpEntity(headers);
+            restTemplate.exchange(
+                    BudgetAdress.getURI() + ":3004/api/account/activationCode",
+                    HttpMethod.POST, httpEntity,Map.class);
+//            restTemplate.exchange(resetPasswordMailUrl,
+//                    HttpMethod.POST,
+//                    new HttpEntity<>(json, headers),
+//                    String.class);
+//            userActivationCodeRepository.save(new UserActivationCode(acc.getId(), token));
+            //===================================================
             try {
                 Map<String, Object> jsonObjects = new LinkedHashMap<>();
                 jsonObjects.put("Account", acc);
