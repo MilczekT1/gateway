f9e3d6b3d390573ef053a308bed41909d7a0157e
Konrad Boniecki <konrad_boniecki@hotmail.com>
2019-08-10 08:52:05 +0000

    Merged in feature/first_route (pull request #9)
    Feature/first route
    
    * basic setup
    
    * use internal port for all services
    
    * springboot 2.1.7

--- src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
+++ src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
@@ -2,18 +2,12 @@ package pl.konradboniecki.budget.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(scanBasePackages = "pl.konradboniecki")
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
