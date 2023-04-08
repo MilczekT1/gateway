e7ce6a8c3c6a0216c9288379def2212ac0da8835
Konrad Boniecki <konrad_boniecki@hotmail.com>
2023-04-08 22:29:05 +0200

    Exclude WebServerAutoConfiguration which contains undertow

--- src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
+++ src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
@@ -2,8 +2,9 @@ package pl.konradboniecki.budget.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.konradboniecki.chassis.configuration.WebServerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = WebServerAutoConfiguration.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
