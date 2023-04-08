69776eaefce900884d5dee2eb7372376631b27d6
Konrad Boniecki <konrad_boniecki@hotmail.com>
2023-04-08 22:46:46 +0200

    fix

--- src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
+++ src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
@@ -2,9 +2,10 @@ package pl.konradboniecki.budget.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.konradboniecki.chassis.configuration.HttpLoggingAutoConfiguration;
 import pl.konradboniecki.chassis.configuration.WebServerAutoConfiguration;
 
-@SpringBootApplication(exclude = WebServerAutoConfiguration.class, HttpLoggingAutoConfiguration.class)
+@SpringBootApplication(exclude = {WebServerAutoConfiguration.class, HttpLoggingAutoConfiguration.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
