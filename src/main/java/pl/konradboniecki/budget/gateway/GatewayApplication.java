81c0f71fde9f7a88fe4cb516e9b9fa268ea6192e
Konrad Boniecki <konrad_boniecki@hotmail.com>
2023-04-08 22:45:10 +0200

    Exclude HttpLoggingAutoConfiguration

--- src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
+++ src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.konradboniecki.chassis.configuration.WebServerAutoConfiguration;
 
-@SpringBootApplication(exclude = WebServerAutoConfiguration.class)
+@SpringBootApplication(exclude = WebServerAutoConfiguration.class, HttpLoggingAutoConfiguration.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
