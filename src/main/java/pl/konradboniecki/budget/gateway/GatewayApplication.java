f13c94346750a16a8fee5822bd2ec8e740c0396d
Konrad Boniecki <konrad_boniecki@hotmail.com>
2023-04-10 12:51:21 +0200

    Chassis adjustment

--- src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
+++ src/main/java/pl/konradboniecki/budget/gateway/GatewayApplication.java
@@ -2,10 +2,15 @@ package pl.konradboniecki.budget.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.konradboniecki.chassis.configuration.DefaultExceptionHandlerAutoConfiguration;
 import pl.konradboniecki.chassis.configuration.HttpLoggingAutoConfiguration;
 import pl.konradboniecki.chassis.configuration.WebServerAutoConfiguration;
 
-@SpringBootApplication(exclude = {WebServerAutoConfiguration.class, HttpLoggingAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        WebServerAutoConfiguration.class,
+        HttpLoggingAutoConfiguration.class,
+        DefaultExceptionHandlerAutoConfiguration.class
+})
 public class GatewayApplication {
 
     public static void main(String[] args) {
